410d4f76dfa5158dcd49ac55217f13ca4addaa88
Thiago Prohaska <thiago.prohaska@gmail.com>
2024-12-26 10:06:34 +0100

    Best Time to Buy and Sell Stock

---
+++ Problems/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,23 @@
+﻿namespace Problems;
+
+public class BestTimeToBuyAndSellStock
+{
+
+    public int MaxProfit(int[] prices)
+    {
+        var maxProfit = 0;
+        var lowerPrice = prices[0];
+
+        foreach (var price in prices)
+        {
+            if(lowerPrice > price)
+                lowerPrice = price;
+
+            if(maxProfit < price - lowerPrice)
+                maxProfit = price - lowerPrice;
+        }
+        
+        return maxProfit;
+    }
+
+}
