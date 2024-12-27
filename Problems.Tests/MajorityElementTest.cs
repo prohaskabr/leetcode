35ad48dd2e9ff77352053523e21c0702793ff703
Thiago Prohaska <thiago.prohaska@gmail.com>
2024-12-27 09:52:59 +0100

    Majority element

---
+++ Problems.Tests/MajorityElementTest.cs
@@ -0,0 +1,26 @@
+﻿namespace Problems.Tests;
+//<summary>
+// Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times.You may assume that the majority element always exists in the array.
+// Example 1:
+//  Input: nums = [3, 2, 3]
+//  Output: 3
+// Example 2:
+//  Input: nums = [2, 2, 1, 1, 1, 2, 2]
+//  Output: 2
+//</summary>
+public class MajorityElementTest
+{
+    [Theory]
+    [InlineData(1, 1, 2, 1, 2, 1, 1)]
+    [InlineData(3, 3, 2, 3)]
+    [InlineData(2, 2, 2, 1, 1, 1, 2, 2)]
+    public void Test(int expected, params int[] numbers)
+    {
+        var stu = new MajorityElement();
+
+        var result = stu.Get(numbers);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Problems/MajorityElement.cs
@@ -0,0 +1,24 @@
+﻿namespace Problems;
+
+public class MajorityElement
+{
+    public int Get(int[] numbers)
+    {
+        var quantity = 0;
+        var majorityItem = 0;
+
+        foreach (var number in numbers)
+        {
+            if (quantity == 0)
+            {
+                majorityItem = number;
+                quantity = 1;
+            }
+            else
+            {
+                quantity += majorityItem == number ? 1 : -1;
+            }
+        }
+        return majorityItem;
+    }
+}
