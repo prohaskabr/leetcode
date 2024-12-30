9d90f7056c83793c29db9d9f4e262c92c7a455f1
Thiago Prohaska <thiago.prohaska@gmail.com>
2024-12-30 11:56:10 +0100

    Add Two Numbers

---
+++ Problems.Tests/AddTwoNumbersTest.cs
@@ -0,0 +1,66 @@
+﻿namespace Problems.Tests;
+
+/// <summary>
+/// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+/// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+///
+/// Example 1:
+/// Input: l1 = [2,4,3], l2 = [5, 6, 4]
+/// Output: [7, 0, 8]
+/// Explanation: 342 + 465 = 807.
+///
+/// Example 2: 
+/// Input: l1 = [0], l2 = [0]
+/// Output: [0]
+/// 
+/// Example 3: 
+/// Input: l1 = [9, 9, 9, 9, 9, 9, 9], l2 = [9, 9, 9, 9]
+/// Output: [8, 9, 9, 9, 0, 0, 0, 1]
+/// </summary>
+public class AddTwoNumbersTest
+{
+    private AddTwoNumbers stu = new();
+
+    [Fact]
+    public void Add_243_to_564_should_be_708()
+    {
+        var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        var result = stu.Add(l1, l2);
+
+        AssertValue(result, 7, 0, 8);
+    }
+
+    [Fact]
+    public void Add_0_to_0_should_be_0()
+    {
+        var l1 = new ListNode(0);
+        var l2 = new ListNode(0);
+
+        var result = stu.Add(l1, l2);
+
+        AssertValue(result, 0);
+    }
+
+    [Fact]
+    public void Add_9999999_to_9999_should_be_89990001()
+    {
+        var l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+        var l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+
+        var result = stu.Add(l1, l2);
+
+        AssertValue(result, 8,9,9,9,0,0,0,1);
+    }
+
+    private void AssertValue(ListNode result, params int[] values)
+    {
+        var node = result;
+        foreach (var value in values)
+        {
+            Assert.Equal(value, node.val);
+            node = node.next;
+        }
+    }
+}
---
+++ Problems/AddTwoNumbers.cs
@@ -0,0 +1,53 @@
+﻿namespace Problems;
+
+public class AddTwoNumbers
+{
+    public ListNode Add(ListNode l1, ListNode l2)
+    {
+        ListNode head = null;
+        ListNode current = null;
+        var firstNumbers = l1;
+        var secondNumbers = l2;
+        var rest = 0;
+
+        while (firstNumbers != null || secondNumbers != null || rest > 0)
+        {
+            var term01 = firstNumbers?.val ?? 0;
+            var term02 = secondNumbers?.val ?? 0;
+
+            var result = term01 + term02 + rest;
+
+            if (result > 9)
+            {
+                rest = result / 10;
+                result %= 10;
+            }
+            else
+            {
+                rest = 0;
+            }
+
+            if (current == null)
+            {
+                head = new ListNode(result);
+                current = head;
+            }
+            else
+            {
+                current.next = new ListNode(result);
+                current = current.next;
+            }
+
+            firstNumbers = firstNumbers?.next;
+            secondNumbers = secondNumbers?.next;
+        }
+
+        return head;
+    }
+}
+
+public class ListNode(int val = 0, ListNode next = null)
+{
+    public int val = val;
+    public ListNode next = next;
+}
