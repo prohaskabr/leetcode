fd20a5be647dbcc84b59858811e3eeffb461d1b8
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-01-03 12:52:06 +0100

    27 Remove Element

---
+++ Problems/RemoveElement.cs
@@ -0,0 +1,20 @@
+﻿namespace Problems;
+
+public class RemoveElement
+{
+    public int Remove(int[] nums, int val)
+    {
+        var position = 0; 
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[position] = nums[i];
+                position++;
+            }
+        }
+
+        return position;
+    }
+}
