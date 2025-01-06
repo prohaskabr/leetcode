d153d077d07842af407ccb99245cf80662c0aa93
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-01-06 11:11:09 +0100

    189 Rotate Array

---
+++ Problems/RotateArray.cs
@@ -0,0 +1,25 @@
+﻿namespace Problems;
+
+public class RotateArray
+{
+    public void Rotate(int[] nums, int k)
+    {
+        var rotate =  k % nums.Length;
+        reverse(nums, 0, nums.Length - 1);
+        reverse(nums, 0, rotate - 1);
+        reverse(nums, rotate, nums.Length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end)
+    {
+
+        while (start < end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
