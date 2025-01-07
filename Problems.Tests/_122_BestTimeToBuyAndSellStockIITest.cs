e07a70d357ad5c3d0040a025a7d3a747aa5387f3
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-01-07 19:16:27 +0100

    122 Best Time to Buy and Sell Stock II

---
+++ Problems.Tests/_122_BestTimeToBuyAndSellStockIITest.cs
@@ -0,0 +1,40 @@
+﻿namespace Problems.Tests;
+
+/// <summary>
+/// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+/// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+/// Find and return the maximum profit you can achieve.
+/// 
+/// Example 1:
+///     Input: prices = [7, 1, 5, 3, 6, 4]
+///     Output: 7
+///     Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+///     Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+///     Total profit is 4 + 3 = 7.
+/// 
+/// Example 2:
+///     Input: prices = [1, 2, 3, 4, 5]
+///     Output: 4
+///     Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+///     Total profit is 4.
+/// 
+/// Example 3:
+///     Input: prices = [7, 6, 4, 3, 1]
+///     Output: 0
+///     Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+/// </summary>
+public class _122_BestTimeToBuyAndSellStockIITest
+{
+    [Theory]
+    [InlineData(7, 7, 1, 5, 3, 6, 4)]
+    [InlineData(4, 1, 2, 3, 4, 5)]
+    [InlineData(0, 7, 6, 4, 3, 1)]
+    public void Test(int expected, params int[] prices)
+    {
+        var stu = new BestTimeToBuyAndSellStockII();
+
+        var result = stu.MaxProfit(prices);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Problems/BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,17 @@
+﻿namespace Problems;
+
+public class BestTimeToBuyAndSellStockII
+{
+    public int MaxProfit(int[] prices)
+    {
+        var profit = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (prices[i] > prices[i - 1])
+                profit += prices[i] - prices[i - 1];
+        }
+
+        return profit;
+    }
+}
