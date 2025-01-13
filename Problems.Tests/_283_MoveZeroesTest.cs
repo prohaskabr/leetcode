23c3c46a715949e815a984e5106f3e7a1ba9a1b6
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-01-13 17:24:51 +0100

    283. Move Zeroes

---
+++ Problems.Tests/_283_MoveZeroesTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Problems.Tests;
+
+public class _283_MoveZeroesTest
+{
+    [Theory]
+    [InlineData(new[] { 1, 3, 12, 0, 0 }, new[] { 0, 1, 0, 3, 12 })]
+    [InlineData(new[] { 0 }, new[] { 0 })]
+    [InlineData(new[] { -1, 0 }, new[] { 0, -1 })]
+    public void Test(int[] expected, int[] nums)
+    {
+        var stu = new MoveZeroes();
+
+        stu.Move(nums);
+
+        Assert.Equal(expected, nums);
+    }
+}
---
+++ Problems/MoveZeroes.cs
@@ -0,0 +1,23 @@
+﻿namespace Problems;
+
+public class MoveZeroes
+{
+    public void Move(int[] nums)
+    {
+        var insertPosition = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[insertPosition] = nums[i];
+                insertPosition++;
+            }
+        }
+
+        for (int i = insertPosition; i < nums.Length; i++)
+        {
+            nums[i] = 0;
+        }
+    }
+}
