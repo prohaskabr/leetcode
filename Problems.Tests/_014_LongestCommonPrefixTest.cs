a382aa874589e529fdc802d6d0bf34d538fbe07a
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-01-19 18:56:43 +0100

    14. Longest Common Prefix

---
+++ Problems.Tests/_014_LongestCommonPrefixTest.cs
@@ -0,0 +1,32 @@
+﻿namespace Problems.Tests;
+
+/// <summary>
+/// Write a function to find the longest common prefix string amongst an array of strings.
+/// If there is no common prefix, return an empty string "".
+/// 
+/// Example 1:
+///     Input: strs = ["flower", "flow", "flight"]
+///     Output: "fl"
+///     
+/// Example 2:
+///     Input: strs = ["dog", "racecar", "car"]
+///     Output: ""
+///     Explanation: There is no common prefix among the input strings.
+/// </summary>
+public class _014_LongestCommonPrefixTest
+{
+
+    [Theory]
+    [InlineData("fl", new[] { "flower", "flow", "flight" })]
+    [InlineData("", new[] { "dog", "racecar", "car" })]
+    [InlineData("", new[] { "abab", "aba", "" })]
+    [InlineData("", new[] { "baab", "bacb", "b", "cbc" })]    
+    public void Test(string expectedPrefix, string[] strs)
+    {
+        var sut = new CommonPrefix();
+
+        var result = sut.LongestCommonPrefix(strs);
+
+        Assert.Equal(expectedPrefix, result);
+    }
+}
---
+++ Problems/CommonPrefix.cs
@@ -0,0 +1,56 @@
+﻿namespace Problems;
+
+public class CommonPrefix
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        int n = strs.Length;
+        for (int i = 0; i < strs[0].Length; ++i)
+        {
+            for (int j = 1; j < n; ++j)
+            {
+                if (i >= strs[j].Length || strs[j][i] != strs[0][i])
+                {
+                    return strs[0].Substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+    public string LongestCommonPrefixV1(string[] strs)
+    {
+        var shortest = strs[0];
+        var size = 0;
+
+        foreach (var str in strs)
+        {
+
+            if (str.Length < shortest.Length)
+                shortest = str;
+        }
+
+        size = shortest.Length;
+
+        foreach (var str in strs)
+        {
+            for (var i = 0; i < Math.Min(str.Length, size); i++)
+            {
+                if (str[i] != shortest[i])
+                {
+                    if (i < size)
+                        size = i;
+
+                    break;
+                }
+
+            }
+
+            if (size == 0)
+                return string.Empty;
+
+        }
+
+        return strs[0][0..size];
+    }
+}
