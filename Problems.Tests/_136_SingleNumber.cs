4454c6c4a903511851f38cb4c7e672e21c5adb90
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-01-23 08:12:57 +0100

    136. Single Number

---
+++ Problems.Tests/_136_SingleNumber.cs
@@ -0,0 +1,17 @@
+﻿namespace Problems.Tests;
+
+public class _136_SingleNumber
+{
+    [Theory]
+    [InlineData(1, new[] { 2, 2, 1 })]
+    [InlineData(4, new[] { 4, 1, 2, 1, 2 })]
+    [InlineData(1, new[] { 1 })]
+    public void Test(int expected, int[] numbers)
+    {
+        var stu = new SingleNumber();
+
+        var result = stu.GetSingleNumber(numbers);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Problems/SingleNumber.cs
@@ -0,0 +1,15 @@
+﻿namespace Problems;
+
+public class SingleNumber
+{
+    public int GetSingleNumber(int[] nums)
+    {
+        int result = 0;
+
+        for (int i = 0; i < nums.Length; i++) {
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+}
