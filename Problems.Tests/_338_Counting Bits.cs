822f8c2ede1f1e9e1fe5aa1e8b416ead695d5a8b
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-01-28 19:07:03 +0100

    338. Counting Bits

---
+++ Problems.Tests/_338_Counting Bits.cs
@@ -0,0 +1,16 @@
+﻿namespace Problems.Tests;
+
+public class _338_Counting_Bits
+{
+    [Theory]
+    [InlineData(new[] { 0, 1, 1 }, 2)]    
+    [InlineData(new[] { 0, 1, 1, 2, 1, 2 }, 5)]    
+    public void Test(int[] expected, int number)
+    {
+        var stu = new NumberOfBits();
+
+        var result = stu.CountBits(number);
+
+        Assert.Equal(expected, result);
+    }
+}
--- Problems/NumberOfBits.cs
+++ Problems/NumberOfBits.cs
@@ -14,4 +14,36 @@ public class NumberOfBits
 
         return bitCount;
     }
+
+    public int[] CountBits(int n)
+    {
+        var result = new int[n + 1];
+        
+        for (int i = 1; i <= n; i++)
+        {
+            result[i] = result[i >> 1] + i % 2;
+        }
+
+        return result;
+    }
+
+    /*public int[] CountBits(int n)
+    {
+        var result = new int[n+1];
+        result[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            var num = i;
+            result[i] = 0;
+            while (num > 0)
+            {
+                if (num % 2 != 0)
+                    result[i] ++;
+
+                num /= 2;
+            }
+        }
+
+        return result;
+    }*/
 }
