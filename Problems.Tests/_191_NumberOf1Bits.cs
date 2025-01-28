64e48dc54cfa581a0b59712230ecee3d93607fff
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-01-28 18:21:07 +0100

    191. Number of 1 Bits

---
+++ Problems.Tests/_191_NumberOf1Bits.cs
@@ -0,0 +1,17 @@
+﻿namespace Problems.Tests;
+
+public class _191_NumberOf1Bits
+{
+    [Theory]
+    [InlineData(3, 11)]
+    [InlineData(1, 128)]
+    [InlineData(30, 2147483645)]
+    public void Test(int expected, int number)
+    {
+        var stu = new NumberOfBits();
+
+        var result = stu.HammingWeight(number);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Problems/NumberOfBits.cs
@@ -0,0 +1,17 @@
+﻿namespace Problems;
+public class NumberOfBits
+{
+    public int HammingWeight(int n)
+    {
+        int bitCount = 0;
+        while (n > 0)
+        {
+            if (n % 2 != 0)
+                bitCount++;
+
+            n /= 2;
+        }
+
+        return bitCount;
+    }
+}
