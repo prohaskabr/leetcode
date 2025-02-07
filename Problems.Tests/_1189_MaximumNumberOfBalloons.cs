f7eac60aaf735f84ef65fbe4f0cdd94a7085fc00
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-02-07 12:27:52 +0100

    1189. Maximum Number of Balloons

---
+++ Problems.Tests/_1189_MaximumNumberOfBalloons.cs
@@ -0,0 +1,22 @@
+﻿namespace Problems.Tests;
+
+/// <summary>
+/// Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+// You can use each character in text at most once.Return the maximum number of instances that can be formed.
+/// </summary>
+public class _1189_MaximumNumberOfBalloons
+{
+    [Theory]
+    [InlineData(1, "nlaebolko")]
+    [InlineData(2, "loonbalxballpoon")]
+    [InlineData(0, "leetcode")]
+    [InlineData(0, "xyz")]
+    public void Test(int expected, string word)
+    {
+        var stu = new MaxNumberOf();
+
+        var result = stu.MaxNumberOfBalloons(word);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Problems/MaxNumberOf.cs
@@ -0,0 +1,44 @@
+﻿namespace Problems;
+
+public class MaxNumberOf
+{
+    public int MaxNumberOfBalloons(string text)
+    {
+        const int BalloonLetters = 7;
+        var result = text.Length / BalloonLetters;
+
+        if (result == 0)
+            return result;
+
+        var map = new Dictionary<char, int>()
+        {
+            ['b'] = 0,
+            ['a'] = 0,
+            ['l'] = 0,
+            ['o'] = 0,
+            ['n'] = 0,
+        };
+
+
+        foreach (var letter in text)
+        {
+            if (map.ContainsKey(letter))
+                map[letter]++;
+        }
+
+        //adjust letter with multiple use
+
+        map['l'] /= 2;
+        map['o'] /= 2;
+
+        foreach (var qtd in map.Values)
+        {
+            if (qtd < result)
+            {
+                result = qtd;
+            }
+        }
+
+        return result;
+    }
+}
