4b4f3034bd673d4c9aeaa2b6d32a28fc48bf0874
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-02-14 14:51:23 +0100

    205. Isomorphic Strings

---
+++ Problems.Tests/_205_IsomorphicStringsTest.cs
@@ -0,0 +1,24 @@
+﻿namespace Problems.Tests;
+
+/// <summary>
+/// Given two strings s and t, determine if they are isomorphic.
+/// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+/// All occurrences of a character must be replaced with another character while preserving the order of characters.
+/// No two characters may map to the same character, but a character may map to itself.
+/// </summary>
+public class _205_IsomorphicStringsTest
+{
+    [Theory]
+    [InlineData(true, "egg", "add")]
+    [InlineData(false, "foo", "bar")]
+    [InlineData(true, "paper", "title")]
+    [InlineData(false, "bbbaaaba", "aaabbbba")]
+    public void Test(bool expected, string first, string second)
+    {
+        var stu = new Isomorphic();
+
+        var result = stu.IsIsomorphic(first, second);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Problems/Isomorphic.cs
@@ -0,0 +1,29 @@
+﻿namespace Problems;
+public class Isomorphic
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        var map = new Dictionary<char, char>();
+
+        if (s.Length != t.Length)
+            return false;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (map.ContainsKey(s[i]))
+            {
+                if (map[s[i]] != t[i])
+                    return false;
+            }
+            else
+            {
+                if (map.ContainsValue(t[i]))
+                    return false;
+
+                map.Add(s[i], t[i]);
+            }
+        }
+
+        return true;
+    }
+}
