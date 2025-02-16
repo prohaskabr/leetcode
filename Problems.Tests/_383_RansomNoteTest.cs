3ffe1b55990e49be6dd33d0ffdd100bb19e6a459
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-02-16 14:30:39 +0100

    383. Ransom Note

---
+++ Problems.Tests/_383_RansomNoteTest.cs
@@ -0,0 +1,33 @@
+﻿namespace Problems.Tests;
+
+/// <summary>
+/// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+/// Each letter in magazine can only be used once in ransomNote.
+/// 
+/// Example 1:
+///     Input: ransomNote = "a", magazine = "b"
+///     Output: false
+/// 
+/// Example 2:
+///     Input: ransomNote = "aa", magazine = "ab"
+///     Output: false
+/// 
+/// Example 3:
+///     Input: ransomNote = "aa", magazine = "aab"
+///     Output: true
+/// </summary>
+public class _383_RansomNoteTest
+{
+    [Theory]
+    [InlineData(false, "a", "b")]
+    [InlineData(false, "aa", "ab")]
+    [InlineData(true, "aa", "aab")]
+    public void Test(bool expected, string first, string second)
+    {
+        var stu = new RansomNote();
+
+        var result = stu.CanConstruct(first, second);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Problems/RansomNote.cs
@@ -0,0 +1,36 @@
+﻿namespace Problems;
+
+public class RansomNote
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        if (ransomNote.Length > magazine.Length)
+            return false;
+
+        var lFounded = 0;
+        var map = new Dictionary<char, int>();
+
+
+        for (int i = 0; i < ransomNote.Length; i++)
+        {
+            if (map.ContainsKey(ransomNote[i]))
+                map[ransomNote[i]]++;
+            else
+                map.Add(ransomNote[i], 1);
+        }
+
+        for (int i = 0; i < magazine.Length; i++)
+        {
+            if (map.ContainsKey(magazine[i]) && map[magazine[i]] > 0)
+            {
+                map[magazine[i]]--;
+                lFounded++;
+            }
+
+            if (lFounded == ransomNote.Length)
+                return true;
+        }
+
+        return false;
+    }   
+}
