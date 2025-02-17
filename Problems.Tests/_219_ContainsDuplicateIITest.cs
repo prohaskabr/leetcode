4edf61ff4ee4d45950d251e6765df7a4f9a6140a
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-02-17 08:36:35 +0100

    219. Contains Duplicate II

---
+++ Problems.Tests/_219_ContainsDuplicateIITest.cs
@@ -0,0 +1,32 @@
+﻿namespace Problems.Tests;
+
+/// <summary>
+/// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+/// 
+/// Example 1:
+///     Input: nums = [1,2,3,1], k = 3
+///     Output: true
+///     
+/// Example 2:
+///     Input: nums = [1,0,1,1], k = 1
+///     Output: true
+///     
+/// Example 3:
+///     Input: nums = [1,2,3,1,2,3], k = 2
+///     Output: false
+/// </summary>
+public class _219_ContainsDuplicateIITest
+{
+    [Theory]
+    [InlineData(true, 3, new[] { 1, 2, 3, 1 })]
+    [InlineData(true, 1, new[] { 1, 0, 1, 1 })]
+    [InlineData(false, 2, new[] { 1, 2, 3, 1, 2, 3 })]
+    public void Test(bool expected, int window, int[] nums)
+    {
+        var stu = new ContainsDuplicate();
+
+        var result = stu.ContainsNearbyDuplicate(nums, window);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Problems/ContainsDuplicate.cs
@@ -0,0 +1,22 @@
+﻿namespace Problems;
+
+public class ContainsDuplicate
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        var map = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (map.Contains(nums[i]))
+                return true;
+            else
+                map.Add(nums[i]);
+
+            if(i >= k)
+                map.Remove(nums[i - k]);
+        }
+
+        return false;
+    }
+}
