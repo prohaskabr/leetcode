0b8ee4dbfcb41abba5406cc53f917efca85bb1b0
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-02-21 08:39:52 +0100

    643. Maximum Average Subarray I

---
+++ Problems.Tests/_643_MaximumAverageSubarrayITest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Tests;
+/// <summary>
+/// You are given an integer array nums consisting of n elements, and an integer k.
+/// Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value.Any answer with a calculation error less than 10-5 will be accepted.
+/// 
+/// Example 1:
+///     Input: nums = [1, 12, -5, -6, 50, 3], k = 4
+///     Output: 12.75000
+///     Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+///     
+/// Example 2:
+///     Input: nums = [5], k = 1
+///     Output: 5.00000
+/// </summary>
+public class _643_MaximumAverageSubarrayITest
+{
+    [Theory]
+    [InlineData(12.75, new[] { 1, 12, -5, -6, 50, 3 },4)]
+    [InlineData(5, new[] { 5 },1)]
+    [InlineData(4, new[] { 0, 4, 0, 3, 2 }, 1)]
+    public void Test(double expected, int[] nums, int window)
+    {
+        var stu = new AverageSubarray();
+
+        var result = stu.FindMaxAverage(nums, window);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Problems/AverageSubarray.cs
@@ -0,0 +1,24 @@
+﻿namespace Problems;
+public class AverageSubarray
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        var result =  0.0;
+
+        for (int i = 0; i < k; i++)
+        {
+            result += nums[i];
+        }
+
+        var current = result;
+
+        for (int i = k; i < nums.Length ; i++) {
+            current = current + nums[i] - nums[i -k];
+
+            if(current > result)
+                result = current;
+        }
+
+        return double.Round(result / k,5);
+    }
+}
